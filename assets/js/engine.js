e85f42a97bb9ccf6d6c39d3f11cdf93bf8291918
mep4741 <mep4741@rit.edu>
2024-02-28 17:39:53 -0500

    created skills, projects section, project detail page

---
+++ assets/js/engine.js
@@ -0,0 +1,55 @@
+// Featured Projects Loading
+function loadProjects() {
+    console.log("here");
+
+    const featuredProjectsDiv = document.getElementById("featuredprojects");
+
+    projects.forEach((project) => {
+        // create html dom element
+        const a = document.createElement("a");
+        a.href = `project.html?tag=${project.tag}`;
+
+        const img = document.createElement("img");
+        img.src = `./assets/media/${project.img}`;
+        img.alt = project.alt;
+
+        // append
+        a.appendChild(img);
+        featuredProjectsDiv.appendChild(a);
+    });
+}
+
+// Load Project Info Detail Page
+function loadProjectDetail() {
+    // get the tag in the url
+    var url_string = window.location;
+    var url = new URL(url_string);
+    var tag = url.searchParams.get("tag");
+
+    // get specific project data
+    var project = projects.filter((obj) => {
+        return obj.tag == tag;
+    });
+
+    // if there are no specific projects, send them back
+    if (project.length < 1) {
+        window.location.href = "home.html";
+    }
+
+    const sectionForData = document.getElementById("projectinfo");
+
+    // create dom elements
+    const img = document.createElement("img");
+    img.src = `./assets/media/${project[0].img}`;
+    img.alt = project[0].alt;
+    sectionForData.appendChild(img)
+
+    const title = document.createElement("h1")
+    title.appendChild(document.createTextNode(project[0].name))
+    sectionForData.appendChild(title)
+
+    const body = document.createElement("p")
+    body.appendChild(document.createTextNode(project[0].desc))
+    sectionForData.appendChild(body)
+
+}
---
+++ assets/js/featuredProjects.js
@@ -0,0 +1,37 @@
+const projects = [
+    {
+        "tag": "raincoat",
+        "name": "Raincoat",
+        "img": "Raincoat.svg",
+        "alt": "Raincoat App, Turn weather into something you can wear",
+        "desc": "What to say about raincoat, hmmm"
+    },
+    {
+        "tag": "retireplan",
+        "name": "RetirePlan",
+        "img": "RetirePlan.svg",
+        "alt": "RetirePlan React App, Learn how your choices affect your savings",
+        "desc": "What to say about RetirePlan, hmmm"
+    },
+    {
+        "tag": "pantry",
+        "name": "Pantry",
+        "img": "Pantry.svg",
+        "alt": "Pantry Flutter App, Store all your recipes, simple, and easily on multiple devices",
+        "desc": "What to say about pantry, hmmm"
+    },
+    {
+        "tag": "coffee",
+        "name": "Coffee Mobile App",
+        "img": "CoffeeMobileApp.svg",
+        "alt": "Coffee Mobile HTML Web App",
+        "desc": "What to say about coffee, hmmm"
+    },
+    {
+        "tag": "ischool",
+        "name": "School of Information Replica Website",
+        "img": "iSchoolReplica.svg",
+        "alt": "School of Information Replica Website, Utilizes React and APIs",
+        "desc": "What to say about ischool, hmmm"
+    }
+]
